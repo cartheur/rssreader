ee4b202bad3f1dc66bc69caff4dbd83569f6a4f6
Cartheur <cartheur@gmail.com>
2019-02-21 11:09:05 +0100

    Update header info

--- MainForm.cs
+++ MainForm.cs
@@ -4,7 +4,7 @@
 // Created          : 18-02-2007
 //
 // Last Modified By : cartheur
-// Last Modified On : 02-21-2019
+// Last Modified On : 21-02-2019
 // ***********************************************************************
 // <copyright file="MainForm.cs" company="cartheur">
 //   2007 - 2019. All rights reserved.
